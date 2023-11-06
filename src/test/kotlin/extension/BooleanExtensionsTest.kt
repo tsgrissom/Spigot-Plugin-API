926c24bb8af18799539687d84160842903c874e0
Tyler Grissom <tylergrissom@me.com>
2023-11-06 10:10:06 -0600

    Re-wrote BooleanExtensionsTest.kt to use ParameterizedTest

--- src/test/kotlin/extension/BooleanExtensionsTest.kt
+++ src/test/kotlin/extension/BooleanExtensionsTest.kt
@@ -6,29 +6,32 @@ import io.github.tsgrissom.pluginapi.extension.kt.fmt
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-
-private fun Boolean.fmtTrueFalse(
-    capitalize: Boolean = false,
-    withColor: Boolean = false
-) : String =
-    this.fmt(BooleanFormat.TRUE_FALSE, capitalize=capitalize, withColor=withColor)
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BooleanExtensionsTest : PAPIPluginTest() {
 
+    private fun Boolean.fmtTrueFalse(
+        capitalize: Boolean = false,
+        withColor: Boolean = false
+    ) : String =
+        this.fmt(BooleanFormat.TRUE_FALSE, capitalize=capitalize, withColor=withColor)
+
     @DisplayName("Does Boolean#fmt with no settings when the receiver is the true literal equal a plain String containing the \"true\" literal?")
-    @Test
-    fun boolFmtNoColorNoCapitalizationOnTrueEqStringLiteralOfTrue() {
-        val tAsStr = "${true}"
-        val tFormattedNoSettings = true.fmtTrueFalse()
-        assertEquals(tFormattedNoSettings, tAsStr)
+    @ParameterizedTest
+    @ValueSource(booleans=[true, false])
+    fun fmt_valueFormattedWithNoCapitalizationAndNoColorShouldEqValueExpressedAsStringLiteral(b: Boolean) {
+        val asStr = "$b"
+        val formatted = b.fmtTrueFalse(capitalize=false, withColor=false)
+        assertEquals(formatted, asStr)
     }
 
     @DisplayName("Does Boolean#fmt with only capitalization enabled when the receiver is the true literal equal a plain String containing the \"true\" literal passed through String#capitalize?")
-    @Test
-    fun boolFmtNoColorCapitalizedEqStringLiteralOfTrueCapitalized() {
-        val tAsStr = "${true}"
-        val capitalizedStr = tAsStr.capitalize()
-        val fmtCapitalized = true.fmtTrueFalse(capitalize=true, withColor=false)
-        assertEquals(capitalizedStr, fmtCapitalized)
+    @ParameterizedTest
+    @ValueSource(booleans=[true, false])
+    fun fmt_valueFormattedWithCapitalizationAndNoColorShouldEqValueExpressedAsStringLiteralCapitalized(b: Boolean) {
+        val asStr = "$b".capitalize()
+        val formatted = b.fmtTrueFalse(capitalize=true, withColor=false)
+        assertEquals(formatted, asStr)
     }
 }
