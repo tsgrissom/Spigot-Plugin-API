5e1b773134db2d415128558c44f3c2d24e96a291
Tyler Grissom <tylergrissom@me.com>
2023-11-06 10:17:47 -0600

    Re-wrote TimeUtilityTest.kt to use ParameterizedTest

--- src/test/kotlin/utility/TimeUtilityTest.kt
+++ src/test/kotlin/utility/TimeUtilityTest.kt
@@ -5,38 +5,42 @@ import io.github.tsgrissom.pluginapi.utility.TimeUtility
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class TimeUtilityTest : PAPIPluginTest() {
 
+    private val util = TimeUtility()
+
     @DisplayName("Does an assortment of valid Strings expressing time in seconds passed to TimeUtility#isInputInSeconds evaluate to true?")
-    @Test
-    fun isInputStringInSeconds() {
-        val util = TimeUtility()
-        fun assert(str: String) =
-            assertTrue(util.isInputInSeconds(str), "$str is not valid input as seconds")
-        val cases = arrayOf("10s", "90000000000s", "1S")
-        cases.forEach { assert(it) }
-    }
+    @ParameterizedTest
+    @ValueSource(strings=[
+        "10s",
+        "90000000000s",
+        "1S"
+    ])
+    fun isInputInSeconds_shouldBeTrueWhenValuesAreValidExpressionsOfTimeInSecondsAsStrings(value: String) =
+        assertTrue(util.isInputInSeconds(value), "$value is not valid input as seconds")
 
     @DisplayName("Does an assortment of valid Strings expressing time in minutes passed to TimeUtility#isInputInMinutes evaluate to true?")
-    @Test
-    fun isInputStringInMinutes() {
-        val util = TimeUtility()
-        fun assert(str: String) =
-            assertTrue(util.isInputInMinutes(str), "$str is not valid input as minutes")
-        val cases = arrayOf("1m", "5M", "10000m")
-        cases.forEach { assert(it) }
-    }
+    @ParameterizedTest
+    @ValueSource(strings=[
+        "1m",
+        "5M",
+        "10000m"
+    ])
+    fun isInputInMinutes_shouldBeTrueWhenValuesAreValidExpressionsOfTimeInMinutesAsStrings(value: String) =
+        assertTrue(util.isInputInMinutes(value), "$value is not valid input as minutes")
 
     @DisplayName("Does an assortment of valid Strings expressing time in hours passed to TimeUtility#isInputInHours evaluate to true?")
-    @Test
-    fun isInputStringInHours() {
-        val util = TimeUtility()
-        fun assert(str: String) =
-            assertTrue(util.isInputInHours(str), "$str is not valid input as minutes")
-        val cases = arrayOf("1h", "5H", "10000h")
-        cases.forEach { assert(it) }
-    }
+    @ParameterizedTest
+    @ValueSource(strings=[
+        "1h",
+        "5H",
+        "10000h"
+    ])
+    fun isInputInHours_shouldBeTrueWhenValuesAreValidExpressionsOfTimeInHoursAsStrings(value: String) =
+        assertTrue(util.isInputInHours(value), "$value is not valid input as hours")
 
     // TODO More tests for 12h and 24h methods
 }
