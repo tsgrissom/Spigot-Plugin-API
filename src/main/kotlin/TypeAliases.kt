00ee5f167cb3a879da9b6c8685640ffdbd258909
Tyler Grissom <tylergrissom@me.com>
2023-11-13 17:12:37 -0600

    Added new TypeAliases

--- src/main/kotlin/TypeAliases.kt
+++ src/main/kotlin/TypeAliases.kt
@@ -1,2 +1,7 @@
+/* Bukkit API */
 typealias BungeeChatColor = net.md_5.bungee.api.ChatColor
 typealias BukkitChatColor = org.bukkit.ChatColor
+
+/* Plugin API */
+typealias Parameter  = io.github.tsgrissom.pluginapi.command.help.SubcParameterBuilder
+typealias SubCommand = io.github.tsgrissom.pluginapi.command.help.SubcHelpBuilder
